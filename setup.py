75bac0cc411f85f859f0f2d78f2260428d4bb445
Uğur Soğukpınar <sogukpinar.ugur@gmail.com>
2018-10-11 13:44:48 +0300

    version upgrade

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="diffcsv",
-    version="0.1",
+    version="0.2",
     author=u"Uğur Soğukpınar",
     author_email="sogukpinar.ugur@gmail.com",
     url="https://github.com/ugursogukpinar/diff-csv",
